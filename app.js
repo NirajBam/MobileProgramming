6b6c4844c1b19ce66bb295a4aec0a9f21d206194
NirajBam <nirajbam125@gmail.com>
2025-11-11 07:56:49 +0545

    week2

---
+++ app.js
@@ -0,0 +1,83 @@
+// // document
+//   // .getElementById("changeStyleBtn")
+//   // .addEventListener("click", function () {
+//   //   document.getElementById("demo").style.fontSize = "35px";
+//   // });
+
+
+// //  function addition() {
+// //    let a = 12;
+// //    let b = 8;
+// //    let c = a + b;
+// //    document.getElementById("add").innerText = "Sum: " + c;
+// //  }
+
+// //  function subtraction() {
+// //    let a = 25;
+// //    let b = 9;
+// //    let c = a - b;
+// //    document.getElementById("sub").innerText = "Difference: " + c;
+// //  }
+
+// //  function multiplication() {
+// //    let a = 6;
+// //    let b = 11;
+// //    let c = a * b;
+// //    document.getElementById("mul").innerText = "Product: " + c;
+// //  }
+
+// //  function division() {
+// //    let a = 45;
+// //    let b = 9;
+// //    let c = a / b;
+// //    document.getElementById("div").innerText = "Quotient: " + c;
+// //  }
+            
+
+// function addition() {
+//   let a = 12;
+//   let b = 8;
+//   let c = a + b;
+//   document.getElementById("add").innerText = "Sum: " + c;
+// }
+
+// function subtraction() {
+//   let a = 25;
+//   let b = 9;
+//   let c = a - b;
+//   document.getElementById("sub").innerText = "Difference: " + c;
+// }
+
+// function multiplication() {
+//   let a = 6;
+//   let b = 11;
+//   let c = a * b;
+//   document.getElementById("mul").innerText = "Product: " + c;
+// }
+
+// function division() {
+//   let a = 45;
+//   let b = 9;
+//   let c = a / b;
+//   document.getElementById("div").innerText = "Quotient: " + c;
+// }
+
+// function ShowTeam() {
+//   let input = document.getElementById("ethnicity").value.trim().toLowerCase();
+//   let img = document.getElementById("displayImage");
+//   let msg = document.getElementById("message");
+
+//   if (input === "Madrid") {
+//     img.src =
+//       "https://thumbs.dreamstime.com/b/fc-real-madrid-la-liga-team-81049265.jpg"; // Real Madrid image
+//     img.style.display = "block";
+//     msg.innerText = "You support Real Madrid ⚪👑";
+//   } else if (input === "non-newari") {
+//     img.src = "images/fcbarcelona.jpg"; // FC Barcelona image
+//     img.style.display = "block";
+//     msg.innerText = "You support FC Barcelona 🔵🔴";
+//   } else {
+//     msg.innerText = "Please enter 'Newari' or 'Non-Newari'.";
+//     img.style.display = "none";
+//   }
+// }
